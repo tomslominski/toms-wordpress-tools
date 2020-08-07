42275bd72fb03ecff1bad52c8ad1644e7d325421
Tom Slominski <tom@slomin.ski>
2020-08-07 17:53:39 +0100

    Cookie module: Typo

--- src/Modules/CookieBanner/Module.php
+++ src/Modules/CookieBanner/Module.php
@@ -30,7 +30,7 @@ class Module extends BaseModule
 	 */
 	public function output_banner()
 	{
-		if ($privacy_policy = get_option('page_for_privacy_policy')): ?>
+		if ($privacy_policy = get_option('wp_page_for_privacy_policy')): ?>
 
 			<div class="ts-cookie-banner ts-hidden">
 				<p><?php printf(__('We use cookies and similar technologies on our website to help us understand how you use it and how we can improve our services. For more information, please read our <a href="%s">privacy policy</a>.', 'toms-wordpress-tools'), get_the_permalink($privacy_policy)); ?></p>
